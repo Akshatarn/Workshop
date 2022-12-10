715a096994574212c0bbcf8668bb80a56945c51d
akshata_rn <akshatarn123@gamail.com>
2022-12-10 08:18:21 +0530

    palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sum = 0, rem, temp = 0;
+            Console.WriteLine("Enter the number to be checked:");
+            num=Convert.ToInt32(Console.ReadLine());
+            temp = num;
+            while(num>0)
+            {
+                rem = num % 10;
+                num = num / 10;
+                sum = sum * 10 + rem;
+            }
+            if(temp == sum)
+            {
+                Console.WriteLine("The given number is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("The given number is not a palindrome");
+            }
+            Console.WriteLine();
+        }
+    }
+}
